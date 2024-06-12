a96132d7633306b47bd4e200e7cafa0d14b020e3
하이 <kkm07133@naver.com>
2024-06-12 22:53:33 +0900

    6/12

--- core1/src/main/java/com/gdsc/core1/AppConfig.java
+++ core1/src/main/java/com/gdsc/core1/AppConfig.java
@@ -5,16 +5,22 @@ import com.gdsc.core1.discount.FixDiscountPolicy;
 import com.gdsc.core1.member.*;
 import com.gdsc.core1.order.OrderService;
 import com.gdsc.core1.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){// 참조한 객체에 생성자를 사용하여 주입
         return new MemberServiceImpl(new MemoryMemberRepository());// 생성자를 통해서 인터페이스를 상속받는 클래스를 주입받음(생성자 주입)
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
