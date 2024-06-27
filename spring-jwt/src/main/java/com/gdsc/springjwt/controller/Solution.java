72b9ae18bfc8ef91758b7e06935317ba640160de
하이 <kkm07133@naver.com>
2024-06-27 12:30:28 +0900

    d

--- spring-jwt/src/main/java/com/gdsc/springjwt/controller/Solution.java
+++ spring-jwt/src/main/java/com/gdsc/springjwt/controller/Solution.java
@@ -1,15 +1,24 @@
-package com.gdsc.springjwt.controller;
-
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
-    public int solution(int[] d, int budget) {
+    public int solution(int[] people, int limit) {
         int answer = 0;
-        d = Arrays.stream(d).sorted().toArray();
-        for(int i = 0;i<d.length;i++){
-            answer += budget % d[i];
-            budget/=d[i];
+        List<Integer> peoples = Arrays.stream(people).sorted().boxed().collect(Collectors.toList());
+        while(peoples.size()>0){
+            if(peoples.get(0) + peoples.get(peoples.size()-1)<=limit){
+                peoples.remove(0);
+                peoples.remove(peoples.size()-1);
+                answer ++;
+            }
+            else {
+                peoples.remove(peoples.size());
+                answer++;
+            }
         }
+
+
         return answer;
     }
 }
