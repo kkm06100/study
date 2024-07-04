54a29260582dc115020b0b48d266a68a1cc9bd8d
하이 <kkm07133@naver.com>
2024-07-04 22:21:53 +0900

    이해한 부분 주석 작성

--- spring-jwt/src/main/java/com/gdsc/springjwt/config/SecurityConfig.java
+++ spring-jwt/src/main/java/com/gdsc/springjwt/config/SecurityConfig.java
@@ -8,42 +8,47 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+@Configuration // 이 클래스가 스프링의 설정 클래스임을 나타낸다.
+@EnableWebSecurity // Spring Security를 활성화 한다
 public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        // 이 메서드는 해당 객체를 빈으로 등록하고 암호화하는데 사용된다.
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        // 이 메소드는 'HttpSecurity' 객체를 사용하여 보안 설정을 구성하고,
+        // 'SecurityFilterChain'을 빈으로 등록한다.
 
-        //csrf disable
+        //csrf 보호를 비활성화 한다. 주로 REST API와 같은 stateless 애플리케이션에서 사용된다.
         http
                 .csrf((auth)->auth.disable());
 
-        //Form 로그인 방식 disable
+        //기본 폼 로그인 방식을 비활성화 한다.
         http
                 .formLogin((auth)->auth.disable());
 
-        //http basic 인증 방식 disable
-        http
-                .formLogin((auth)->auth.disable());
-
+        //요청에 대한 관한을 설정한다.
         http
                 .authorizeHttpRequests((auth)->auth
                         .requestMatchers("/login","/","join").permitAll()
+                        // 해당 경로에 대한 모든 접근을 허용한다.
                         .requestMatchers("/admin").hasRole("ADMIN")
+                        // "/admin"이라는 경로는 역할이 "ADMIN"인 사람만 접근할 수 있다.
                         .anyRequest().authenticated());
+                        // 다른 모든 요청은 인증된 사용자만 접근할 수 있다.
 
         //세션 설정
         http
                 .sessionManagement((session)->session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션 : STATELESS
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        // 세션 : STATELESS, 비활성화 시킴
 
         return http.build();
+        // 필터체인에 결과를 반환
     }
 }
 
