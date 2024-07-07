536bdf0e0a67d2219dd3a8cc46bbfac4cc4074cd
하이 <kkm07133@naver.com>
2024-07-07 23:14:48 +0900

    주석 -  CustomUserDetails

--- spring-jwt/src/main/java/com/gdsc/springjwt/dto/CustomUserDetails.java
+++ spring-jwt/src/main/java/com/gdsc/springjwt/dto/CustomUserDetails.java
@@ -8,18 +8,23 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class CustomUserDetails implements UserDetails {
+    // Spring Security 전용 Dto + UserDetails 상속
 
-    private final User user;
+    private final User user; // User사용
     public CustomUserDetails(User user){
         this.user = user;
     }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        // <? extends GrantedAuthority>는 와일드카드 제네릭을 사용하여 GrantedAuthority를 상속하거나 구현하는 모든 타입을 허용한다.
+        // 와일드 카드 : 타입에 대한 제한 X
+        // 즉, GrantedAuthority의 하위 타입들을 포함할 수 있다.
+
         Collection<GrantedAuthority> collection = new ArrayList<>();
-        collection.add(new GrantedAuthority() {
+        collection.add(new GrantedAuthority() { // 익명 클래스
             @Override
             public String getAuthority() {
-                return user.getRole();
+                return user.getRole(); // 권한을 반환
             }
         });
         return collection;
@@ -27,11 +32,11 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return user.getPassword(); // 비밀번호를 반환
     }
 
     @Override
     public String getUsername() {
-        return user.getUsername();
+        return user.getUsername(); // 이름을 반환
     }
 }
