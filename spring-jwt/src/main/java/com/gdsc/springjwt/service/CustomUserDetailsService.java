ed70066e5040f42d0dab970bd3554022360c50b6
하이 <kkm07133@naver.com>
2024-07-07 23:18:42 +0900

    주석 - CustomUserDetailsService

--- spring-jwt/src/main/java/com/gdsc/springjwt/service/CustomUserDetailsService.java
+++ spring-jwt/src/main/java/com/gdsc/springjwt/service/CustomUserDetailsService.java
@@ -7,18 +7,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService { // UserDeatilsService에 정의된 기능을 사용
 
     private final UserRepository userRepository;
+    // UserRepository 사용
     public CustomUserDetailsService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User userData = userRepository.findByUsername(username);
+        User userData = userRepository.findByUsername(username); // username을 바탕으로 객체 생성
 
         if(userData != null){
-            return new CustomUserDetails(userData);
+            return new CustomUserDetails(userData); // null이 아니라면 반환
         }
         return null;
     }
