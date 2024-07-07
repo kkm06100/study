54df080a47904f6bacd5a9757cf4569ff805cee5
하이 <kkm07133@naver.com>
2024-07-07 23:05:14 +0900

    주석 작성-JoinService

--- spring-jwt/src/main/java/com/gdsc/springjwt/service/JoinService.java
+++ spring-jwt/src/main/java/com/gdsc/springjwt/service/JoinService.java
@@ -13,6 +13,7 @@ public class JoinService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
+        // userRepository, BCryptPasswordEncoder를 생성자 주입
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
@@ -21,13 +22,14 @@ public class JoinService {
         String password = joinDto.getPassword();
 
         Boolean isExist = userRepository.existsByUsername(username);
+        // username이 존재하는지 확인(true, false 형태로 저장)
 
-        if(isExist){
+        if(isExist){ // 존재한다면 메소드 종료
 
             return;
         }
 
-        User data = new User();
+        User data = new User(); // 없는 경우 새로운 객체 생성 + 저장
 
         data.setUsername(username);
         data.setPassword(bCryptPasswordEncoder.encode(password));
