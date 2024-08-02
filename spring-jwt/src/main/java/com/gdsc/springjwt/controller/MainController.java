83faac956c804a1b0d3be6ac6986c689f5f87a65
하이 <kkm07133@naver.com>
2024-08-02 11:47:13 +0900

    1

--- spring-jwt/src/main/java/com/gdsc/springjwt/controller/MainController.java
+++ spring-jwt/src/main/java/com/gdsc/springjwt/controller/MainController.java
@@ -1,10 +1,10 @@
 package com.gdsc.springjwt.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @ResponseBody
 public class MainController {
     @GetMapping("/")
